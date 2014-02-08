4ff2e32152a673f99afe1f5689aa63368f9e73a0
Abhijit Navale <abhijit.foss@gmail.com>
2014-02-08 17:40:14 +0530

    Update campagne_campaigns_controller.rb

--- app/controllers/campagne/campagne_campaigns_controller.rb
+++ app/controllers/campagne/campagne_campaigns_controller.rb
@@ -61,6 +61,6 @@ module Campagne
   end
   private
 	def project_params
-		params.require(:CampagneCampaign).permit(:id,:campagne_campagne_campaign,:schedule,:location,:notice,:layout)
+		params.require(:CampagneCampaigns).permit(:id,:campagne_campagne_campaign,:schedule,:location,:notice,:layout)
 	end
 end
