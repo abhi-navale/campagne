5bf3f692e268d3d22ad068724b8ac1ae98dae308
Abhijit Navale <abhijit.foss@gmail.com>
2014-02-09 21:41:11 +0530

    Update campagne_contact.rb

--- app/models/campagne/campagne_contact.rb
+++ app/models/campagne/campagne_contact.rb
@@ -5,7 +5,7 @@ module Campagne
     #attr_accessible :email, :name
 
     validates :email, :presence => true, :uniqueness => true
-    validates_format_of :email, :with => \A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$\z
+    validates_format_of :email, :with => \A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$\z/i
 
   end
 end
