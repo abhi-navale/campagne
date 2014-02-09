a0a820987b5de1326eb3d19ea8749e7c06e84f5e
Abhijit Navale <abhijit.foss@gmail.com>
2014-02-09 21:49:58 +0530

    Update campagne_contact.rb

--- app/models/campagne/campagne_contact.rb
+++ app/models/campagne/campagne_contact.rb
@@ -5,7 +5,8 @@ module Campagne
     #attr_accessible :email, :name
 
     validates :email, :presence => true, :uniqueness => true
-    validates_format_of :email, :with => \A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$\z/i
+    #validates_format_of :email, :with => \A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$\z/i
+    validates :email, email_format: { message: "Not a valid email address" }
 
   end
 end
