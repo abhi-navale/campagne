30701b788e2661de2a04a2c51de36eb381243c6c
Abhijit Navale <abhijit.foss@gmail.com>
2014-02-10 21:25:56 +0530

    Update campagne_campaigns_controller.rb

--- app/controllers/campagne/campagne_campaigns_controller.rb
+++ app/controllers/campagne/campagne_campaigns_controller.rb
@@ -13,7 +13,7 @@ module Campagne
     end
 
     def new
-      @campaign = CampagneCampaign.new(params[:CampagneCampaigns].permit(:name,:subject,:body))
+      @campaign = CampagneCampaign.new(params[:project_params].permit(:name,:subject,:body))
       respond_with(@campaign)
     end
 
